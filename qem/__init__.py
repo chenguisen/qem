37aa97170bc4b083087f728765c00bab81ca1cd0
zezhong-zhang <jack.zezhong.zhang@gmail.com>
2025-07-08 13:24:04 +0800

    set backend in the .env

--- qem/__init__.py
+++ qem/__init__.py
@@ -1,4 +1,4 @@
 from . import io
 from .benchmark import Benchmark
 from .crystal_analyzer import CrystalAnalyzer
-from .image_fitting import ImageModelFitting
+from .image_fitting import ImageFitting
